b52e13219c1a91f81e960d588d22f2da3bc2aab0
ERFAN <erfan.shokr300@gmail.com>
2024-12-07 20:17:37 +0300

    In this section we done array how create list what is syntax creator list in GO language and else...

--- 01_hello/main.go
+++ 01_hello/main.go
@@ -3,25 +3,28 @@ package main
 import "fmt"
 
 func main() {
-	book := "The Colour of Magic"
+	loons := []string{"bugs", "daffy", "taz"}
+	fmt.Printf("foo: %v type of %T\n", loons, loons)
 
-	fmt.Println(book)
-	fmt.Println(len(book))
+	//lenght
+	fmt.Println(len(loons))
+	// index
+	fmt.Println(loons[1])
+	// slice
+	fmt.Println(loons[1:])
 
-	fmt.Printf("book[0] = %v (type %T)\n", book[0], book[0])
+	// for loop
+	for i := 0; i < len(loons); i++ {
+		fmt.Println(loons[i])
+	}
 
-	fmt.Println(book[4:11])
-	fmt.Println(book[4:])
-	fmt.Println(book[:4])
+	// for value range
+	for index := range loons {
+		fmt.Println(index)
+	}
 
-	fmt.Println("hi " + book)
-
-	// multiline string
-	poem := `
-The road goes ever on
-Down from the door where it began
-Now far ahead the road has gone
-...
-`
-	fmt.Println(poem)
+	// for double value range
+	for index, value := range loons {
+		fmt.Printf("%s - %d\n", value, index)
+	}
 }
