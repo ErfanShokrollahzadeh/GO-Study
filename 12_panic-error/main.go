9d1cac50e7343a3e4e55efebc22dc39f6af41ac0
ERFAN <erfan.shokr300@gmail.com>
2025-02-02 18:14:39 +0300

    Implement safe value retrieval with panic recovery in main function

--- 12_panic-error/main.go
+++ 12_panic-error/main.go
@@ -1,11 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	// "os"
+)
+
+
+func safevalue(vals []int, index int) int {
+	defer func () {
+		if err:= recover(); err != nil {
+			fmt.Printf("panic occured: %v\n", err)
+		}
+	}()
+
+	return vals[index] 
+		
+}
+
 
 
 func main(){
-	vals := []int{1,2,3}
-	// panic: runtime error: index out of range [3] with length 3
-	v := vals[10]
+	// vals := []int{1,2,3}
+	// // panic: runtime error: index out of range [3] with length 3
+	// v := vals[10]
+	// fmt.Println(v)
+
+	// file, err := os.Open("no-such-file")
+
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// defer file.Close()
+	// fmt.Println("file opened")
+
+
+	v := safevalue([]int{1,2,3}, 10)
 	fmt.Println(v)
 }
